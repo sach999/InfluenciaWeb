78de9919cfc90c2f5e62dc10ac96e84e3fb31aa9
Sachin RS <43383547+sach999@users.noreply.github.com>
2019-09-19 20:41:58 +0530

    Update signedIn.js

--- CodeFury/SignedIn/signedIn.js
+++ CodeFury/SignedIn/signedIn.js
@@ -1,13 +1,4 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBd77_DiSvrirrZ1FjRCkpIO_7rJZAv7tY",
-    authDomain: "influencia-ffaad.firebaseapp.com",
-    databaseURL: "https://influencia-ffaad.firebaseio.com",
-    projectId: "influencia-ffaad",
-    storageBucket: "",
-    messagingSenderId: "886560911024",
-    appId: "1:886560911024:web:8840b5a9941ef941985575"
-};
-firebase.initializeApp(firebaseConfig);
+
 
 
 firebase.auth().onAuthStateChanged(function (user) {
